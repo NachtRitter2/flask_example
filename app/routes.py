619bbf1162f43a2bb958558524b55f21eccf9593
Mike Donlon <nachtritter2@gmail.com>
2022-11-24 18:08:44 -0800

    added config, forms, database

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -18,21 +19,14 @@ def index():
             ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route("/page1")
-def page1():
-
-    user = {'username':'Todd'}
-    posts = [
-            {
-                'author':{'username':'Jeff'},
-                'body':'Rainy day in Sacramento!'
-            },
-            {
-                'author':{'username':'Sherri'},
-                'body':'The art gallery visit was Enchanting!'
-            }
-            ]
-    return render_template('index.html', title='Page1', user=user, posts=posts)
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 
