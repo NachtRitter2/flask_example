70ac42b1dbee2dc6d4faf4cc7d1e3c8ed96923a2
Mike Donlon <nachtritter2@gmail.com>
2022-11-25 21:27:47 -0800

    Added error handling & custom error pages

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,18 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from werkzeug.debug import DebuggedApplication
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# app.debug = True
+# app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+
+
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
