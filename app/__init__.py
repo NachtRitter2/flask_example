45792432eb0e29931aaf25a909fe3544386c629d
Mike Donlon <nachtritter2@gmail.com>
2022-11-25 23:38:05 -0800

    Added error handling, logging. Fixed ownership

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
 from werkzeug.debug import DebuggedApplication
 
 app = Flask(__name__)
@@ -13,8 +19,34 @@ migrate = Migrate(app, db)
 # app.debug = True
 # app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
-
 login = LoginManager(app)
 login.login_view = 'login'
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='Trakr site failure',
+                credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    if not os.path.exists(os.path.join(basedir,'..','logs')):
+        os.mkdir(os.path.join(basedir,'..','logs'))
+    file_handler = RotatingFileHandler(os.path.join(basedir,'..','logs/trakr.log'), maxBytes=10240,
+            backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Trakr Startup')
 
 from app import routes, models, errors
