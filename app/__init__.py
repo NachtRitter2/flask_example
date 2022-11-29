39d9fe862b05def12018c973abc6cf9a4d2cc42c
Mike Donlon <nachtritter2@gmail.com>
2022-11-28 21:18:16 -0800

    Updated all styling with Flask Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap4
 
 from werkzeug.debug import DebuggedApplication
 
@@ -17,6 +18,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+bootstrap = Bootstrap4(app)
 
 # app.debug = True
 # app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
