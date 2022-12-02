c0306b4544c0ff15d3efcb47cea5b414c05d61bb
Mike Donlon <nachtritter2@gmail.com>
2022-12-01 23:42:05 -0800

    Added flask command line shortcut definitions for babel

--- trakr.py
+++ trakr.py
@@ -1,6 +1,6 @@
-from app import app, db
+from app import app, db, cli
 from app.models import User, Post
 
 @app.shell_context_processor
-dev make_shell_context():
+def make_shell_context():
     return {'db':db, 'User':User, 'Post':Post}
