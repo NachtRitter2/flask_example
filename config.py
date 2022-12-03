1e782bdfbaf57ccde84858a6af162c90452cbccf
Mike Donlon <nachtritter2@gmail.com>
2022-12-02 23:38:37 -0800

    Preparing Azure translator interaction

--- config.py
+++ config.py
@@ -15,5 +15,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ADMINS = ['mike@thedonlonfamily.com']
     LANGUAGES = ['en', 'de']
