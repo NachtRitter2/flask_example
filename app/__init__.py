d48e18bfd717f1d70349277f4652f612f3734b33
Mike Donlon <nachtritter2@gmail.com>
2022-12-09 00:08:44 -0800

    Made some fixes to the app init

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
 from flask import Flask, request, current_app
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -11,6 +10,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap4
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+from config import Config
 from werkzeug.debug import DebuggedApplication
 
 db = SQLAlchemy()
@@ -30,7 +30,7 @@ login.login_message = _l('Please log in to access this page.')
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    current_app.config.from_object(config_class)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -45,6 +45,9 @@ def create_app(config_class=Config):
 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
 
     if not app.debug and not app.testing:
